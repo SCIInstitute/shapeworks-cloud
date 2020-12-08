0a8ea73e3e4d7a04b959abd1035b41a0a142c832
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-12-08 16:04:32 -0500

    Add terraform files

---
+++ terraform/heroku.tf
@@ -0,0 +1,17 @@
+data "heroku_team" "heroku" {
+  # This must be created by hand in the Heroku console
+  name = "shapeworks-cloud"
+}
+
+module "django" {
+  source  = "girder/django/heroku"
+  version = "0.5.0"
+
+  project_slug     = "shapeworks-cloud"
+  route53_zone_id  = data.aws_route53_zone.shapeworks_cloud.zone_id
+  heroku_team_name = data.heroku_team.heroku.name
+  subdomain_name   = "app"
+
+  heroku_postgresql_plan = "hobby-basic"
+  heroku_worker_dyno_quantity = 0
+}

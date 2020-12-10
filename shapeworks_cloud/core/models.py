7a9f1d655ac963b8f8419295c5f219d5dcffc20a
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-12-10 18:19:46 -0500

    Rearrange models

--- shapeworks_cloud/core/models.py
+++ shapeworks_cloud/core/models.py
@@ -2,23 +2,18 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 from s3_file_field import S3FileField
 
-ASSET_TYPE_CHOICES = [
-    ('segmentation', 'Segmentation'),
-    ('groomed', 'Groomed'),
-    ('shape_model', 'Shape Model'),
-]
-
 
 class Dataset(TimeStampedModel, models.Model):
     name = models.CharField(max_length=255)
     # TimeStampedModel also provides "created" and "modified" fields
 
 
-class Asset(TimeStampedModel, models.Model):
+class BlobModel(TimeStampedModel, models.Model):
     name = models.CharField(max_length=255)
     blob = S3FileField()
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='files')
-    asset_type = models.CharField(max_length=20, choices=ASSET_TYPE_CHOICES)
+
+    class Meta:
+        abstract = True
 
     @property
     def formatted_size(self, base=1024, unit='B'):
@@ -31,3 +26,24 @@ class Asset(TimeStampedModel, models.Model):
             size /= base
             i += 1
         return f'{size:.2f} {units[i]}{unit}'
+
+
+class Segmentation(BlobModel):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='segmentations')
+
+
+class Groomed(BlobModel):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='groomed')
+
+
+class ShapeModel(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=255)
+    analyze = S3FileField()
+    correspondence = S3FileField()
+    transform = S3FileField()
+    magic_number = models.IntegerField()
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='shape_models')
+
+
+class ShapeModelBlob(BlobModel):
+    shape_model = models.ForeignKey(ShapeModel, on_delete=models.CASCADE, related_name='blobs')

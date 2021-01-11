8d143b90e18d1dada8f98e0c9d913daee3701a86
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-01-11 13:11:39 -0500

    Add patterns to dataset and metadata to blob models

--- shapeworks_cloud/core/models.py
+++ shapeworks_cloud/core/models.py
@@ -2,18 +2,36 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 from s3_file_field import S3FileField
 
+from .metadata import METADATA_FIELDS, generate_filename
+
 
 class Dataset(TimeStampedModel, models.Model):
     name = models.CharField(max_length=255)
-    # TimeStampedModel also provides "created" and "modified" fields
+    groomed_pattern = models.CharField(max_length=255, null=False, blank=True, default='')
+    segmentation_pattern = models.CharField(max_length=255, null=False, blank=True, default='')
+    particles_pattern = models.CharField(max_length=255, null=False, blank=True, default='')
 
 
 class BlobModel(TimeStampedModel, models.Model):
-    name = models.CharField(max_length=255)
     blob = S3FileField()
+
+    # Each member of METADATA_FIELDS has a corresponding field here
+    subject = models.IntegerField(null=False)
 
     class Meta:
         abstract = True
+
+    @property
+    def pattern(self):
+        raise NotImplementedError()
+
+    @property
+    def metadata(self):
+        return {field: self.__dict__[field] for field in METADATA_FIELDS}
+
+    @property
+    def name(self):
+        return generate_filename(self.pattern, self.metadata)
 
     @property
     def formatted_size(self, base=1024, unit='B'):
@@ -29,11 +47,25 @@ class BlobModel(TimeStampedModel, models.Model):
 
 
 class Segmentation(BlobModel):
+    class Meta(BlobModel.Meta):
+        unique_together = METADATA_FIELDS + ['dataset']
+
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='segmentations')
+
+    @property
+    def pattern(self):
+        return self.dataset.segmentation_pattern
 
 
 class Groomed(BlobModel):
+    class Meta(BlobModel.Meta):
+        unique_together = METADATA_FIELDS + ['dataset']
+
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='groomed')
+
+    @property
+    def pattern(self):
+        return self.dataset.groomed_pattern
 
 
 class ShapeModel(TimeStampedModel, models.Model):
@@ -46,4 +78,11 @@ class ShapeModel(TimeStampedModel, models.Model):
 
 
 class Particles(BlobModel):
+    class Meta(BlobModel.Meta):
+        unique_together = METADATA_FIELDS + ['shape_model']
+
     shape_model = models.ForeignKey(ShapeModel, on_delete=models.CASCADE, related_name='particles')
+
+    @property
+    def pattern(self):
+        return self.shape_model.dataset.particles_pattern

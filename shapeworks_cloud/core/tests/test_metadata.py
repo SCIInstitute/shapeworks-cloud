09cfa9b6950b18b5972074786f2b650e0a90476c
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-01-13 12:18:03 -0500

    Update metadata helper
    * Unique regexes per field
    * More fields: grooming_steps, chirality, and extension
    * Separate field validation methods
    * More tests

--- shapeworks_cloud/core/tests/test_metadata.py
+++ shapeworks_cloud/core/tests/test_metadata.py
@@ -9,10 +9,14 @@ from shapeworks_cloud.core import metadata
     'pattern,regex',
     [
         (r'abc', r'abc'),
-        (r'{subject}', r'(?P<subject>\S+)'),
-        (r'{subject:x}', r'(?P<subject>\S+)'),
-        (r'pre{subject}post', r'pre(?P<subject>\S+)post'),
-        (r'pre{subject:x}post', r'pre(?P<subject>\S+)post'),
+        (r'{subject}', r'(?P<subject>[0-9]+)'),
+        (r'{particle_type}', r'(?P<particle_type>local|world|wptsFeatures)'),
+        (r'{chirality}', r'(?P<chirality>L|R)'),
+        (r'{extension}', r'(?P<extension>nrrd|vtk|ply|particles)'),
+        (r'{grooming_steps}', r'(?P<grooming_steps>[a-zA-Z]+(?:\.[a-zA-Z]+)*)'),
+        (r'{subject:x}', r'(?P<subject>[0-9]+)'),
+        (r'pre{subject}post', r'pre(?P<subject>[0-9]+)post'),
+        (r'pre{subject:x}post', r'pre(?P<subject>[0-9]+)post'),
         (r'{undefined}', r'{undefined}'),
     ],
 )
@@ -30,7 +34,23 @@ def test_pattern_as_regex_double_definitions():
     [
         (r'abc', r'abc', {}),
         (r'{subject}', r'1', {'subject': 1}),
+        (r'{particle_type}', r'local', {'particle_type': 'local'}),
+        (r'{chirality}', r'R', {'chirality': 'R'}),
+        (r'{extension}', r'vtk', {'extension': 'vtk'}),
+        (r'{grooming_steps}', r'a', {'grooming_steps': 'a'}),
+        (r'{grooming_steps}', r'a.b.C.deFgHij', {'grooming_steps': 'a.b.C.deFgHij'}),
         (r'ellipsoid_{subject}_L.nrrd', 'ellipsoid_42_L.nrrd', {'subject': 42}),
+        (
+            r'{subject}{chirality}.{grooming_steps}_{particle_type}.{extension}',
+            r'6L.isores.center.pad.com.aligned.cropped.tpSmoothDT_local.vtk',
+            {
+                'subject': 6,
+                'chirality': 'L',
+                'particle_type': 'local',
+                'extension': 'vtk',
+                'grooming_steps': 'isores.center.pad.com.aligned.cropped.tpSmoothDT',
+            },
+        ),
     ],
 )
 def test_extract_metadata(pattern, filename, expected):
@@ -42,7 +62,12 @@ def test_extract_metadata(pattern, filename, expected):
     [
         (r'{subject}{subject}', None, 'Multiple definitions of subject'),
         (r'a', r'b', 'File b does not match pattern a'),
-        (r'{subject}', r'foo', "invalid literal for int() with base 10: 'foo'"),
+        (r'{subject}', r'foo', r'File foo does not match pattern {subject}'),
+        (r'{particle_type}', r'extreme', r'File extreme does not match pattern {particle_type}'),
+        (r'{chirality}', r'Up', r'File Up does not match pattern {chirality}'),
+        (r'{extension}', r'txt', r'File txt does not match pattern {extension}'),
+        (r'{grooming_steps}', r'_', r'File _ does not match pattern {grooming_steps}'),
+        (r'{grooming_steps}', r'x' * 256, r'grooming_steps is more than 255 characters'),
     ],
 )
 def test_extract_metadata_error(pattern, filename, expected):
@@ -74,7 +99,7 @@ def test_validate_filename(pattern, filename):
     [
         (r'{subject}{subject}', None, 'Multiple definitions of subject'),
         (r'a', r'b', 'File b does not match pattern a'),
-        (r'{subject}', r'foo', "invalid literal for int() with base 10: 'foo'"),
+        (r'{subject}', r'foo', r'File foo does not match pattern {subject}'),
         (r'{subject:03}', '1', '1 does not match generated 001'),
     ],
 )
@@ -101,7 +126,7 @@ def test_validate_metadata(pattern, _metadata):
     'pattern,_metadata,expected',
     [
         (r'{subject}{subject}', {'subject': None}, 'Multiple definitions of subject'),
-        (r'{subject}', {'subject': 'foo'}, "invalid literal for int() with base 10: 'foo'"),
+        (r'{subject}', {'subject': 'foo'}, r'File foo does not match pattern {subject}'),
     ],
 )
 def test_validate_metadata_error(pattern, _metadata, expected):

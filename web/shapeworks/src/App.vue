cfb83712a4aff7939fed3acfdeb3fb72e11bc531
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-04 12:45:01 -0400

    Add navbar component

--- web/shapeworks/src/App.vue
+++ web/shapeworks/src/App.vue
@@ -1,42 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <nav-bar />
     <v-main>
       <shape-viewer
         :data="shapeData"
@@ -49,6 +13,7 @@
 </template>
 
 <script>
+import NavBar from './components/NavBar';
 import ShapeViewer from './components/ShapeViewer';
 import shapeReader from './reader/shape';
 import pointsReader from './reader/points';
@@ -91,6 +56,7 @@ const POINTS_URLS = [
 
 export default {
   components: {
+    NavBar,
     ShapeViewer,
   },
   data() {
---
+++ web/shapeworks/src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+    </div>
+    <v-tabs>
+      <v-tab>
+        Data
+      </v-tab>
+      <v-tab>
+        Groom
+      </v-tab>
+      <v-tab>
+        Optimize
+      </v-tab>
+      <v-tab>
+        Analyze
+      </v-tab>
+    </v-tabs>
+  </v-app-bar>
+</template>
+
+<style>
+</style>

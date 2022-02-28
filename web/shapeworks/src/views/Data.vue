05ca58e40df89ba05bfd9683ee417c5dff92d69c
Anne Haley <anne.haley@kitware.com>
2022-02-28 10:00:04 -0500

    Add context info to top of Data tab

--- web/shapeworks/src/views/Data.vue
+++ web/shapeworks/src/views/Data.vue
@@ -1,7 +1,53 @@
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api';
+import { selectedDataset, selectedSubject } from '../store';
+
+
+export default defineComponent({
+    setup() {
+        onMounted(async () => {
+            if (!selectedSubject.value) return;
+        })
+
+        return {
+            selectedDataset,
+            selectedSubject,
+        }
+    }
+})
+</script>
+
+
 <template>
-  <div>Data</div>
+    <div>
+        <div class="context-card">
+            <div>
+                <div class="text-overline">
+                    DATASET ({{ selectedDataset.created.split('T')[0] }})
+                </div>
+                <v-list-item-title class="text-h6 mb-1">
+                    {{ selectedDataset.name }}
+                </v-list-item-title>
+            </div>
+            <div>
+                <div class="text-overline">
+                    SUBJECT ({{ selectedSubject.created.split('T')[0] }})
+                </div>
+                <v-list-item-title class="text-h6 mb-1">
+                    {{ selectedSubject.name }}
+                </v-list-item-title>
+            </div>
+        </div>
+        <v-divider />
+
+    </div>
 </template>
 
-<script lang="ts">
-export default {}
-</script>
+<style scoped>
+.context-card {
+    display: flex;
+    column-gap: 40px;
+    padding: 10px 20px;
+    width: 100%
+}
+</style>

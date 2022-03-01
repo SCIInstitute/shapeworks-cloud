e0835897189573b8d4789d54b226241a27e2c4bf
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-03-01 16:22:38 -0500

    Add store helper to load dataset+subject

--- web/shapeworks/src/store.ts
+++ web/shapeworks/src/store.ts
@@ -1,4 +1,5 @@
 import { DataObject, Dataset, Subject } from '@/types'
+import { getDataset, getSubject } from '@/api/rest';
 import { ref } from '@vue/composition-api'
 
 export const loadingState = ref<boolean>(false)
@@ -14,3 +15,13 @@ export const selectedSubject = ref<Subject>()
 export const allDataObjectsForSubject = ref<DataObject[]>([])
 
 export const selectedDataObjects = ref<DataObject[]>([])
+
+export const loadDatasetAndSubject = async (datasetId: number, subjectId: number) => {
+    // Only reload if something has changed
+    if (selectedDataset.value?.id != datasetId || selectedSubject.value?.id != subjectId) { 
+        loadingState.value = true;
+        selectedDataset.value = await getDataset(datasetId);
+        selectedSubject.value = await getSubject(subjectId);
+        loadingState.value = false;
+    }
+}

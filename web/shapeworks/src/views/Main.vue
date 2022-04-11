31a0bf0d347b96b7cf14d809e29566d7e3559bd9
Anne Haley <anne.haley@kitware.com>
2022-04-11 12:22:03 -0400

    Fix label bug

--- web/shapeworks/src/views/Main.vue
+++ web/shapeworks/src/views/Main.vue
@@ -82,7 +82,7 @@ export default defineComponent({
                         )))
                         .map(([imageData, particleData]) => ({shape: imageData, points: particleData}))
                         return [
-                            subjectName, shapeDatas
+                            `${subjectName} - ${geometryShown.value}`, shapeDatas
                         ]
                     }
                 )

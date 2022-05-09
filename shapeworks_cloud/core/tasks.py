6057654b6566d375c5693e4ce4bd17c014235dbb
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-05-09 10:54:07 -0400

    Add rudimentary groom task

--- shapeworks_cloud/core/tasks.py
+++ shapeworks_cloud/core/tasks.py
@@ -2,77 +2,30 @@
 # from pathlib import Path
 # from tempfile import TemporaryDirectory
 # from typing import List
+from subprocess import Popen, PIPE
 
-# from celery import shared_task
-# from django.core.files.base import ContentFile
-# import numpy as np
+from celery import shared_task
+from django.conf import settings
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# from . import models, serializers
-# from .shapeworks_interface.convert import nrrd_to_vtp
-# from .shapeworks_interface.optimize import optimize as _optimize
-
-
-# @shared_task
-# def optimize_shapes(optimization_id: int):
-#     """Perform the optimization step for an initialized project."""
-#     optimization: models.Optimization = models.Optimization.objects.get(pk=optimization_id)
-#     project = optimization.project
-
-#     optimization.running = True
-#     optimization.save()
-
-#     try:
-#         parameters = serializers.OptimizationParametersSerializer(optimization.parameters).data
-#         images: List[Path] = []
-#         with TemporaryDirectory() as _dir:
-#             dir = Path(_dir)
-#             for image in project.groomed_dataset.segmentations.all().order_by('index'):
-#                 # TODO: for very large images we would want to stream this to the filesystem
-#                 data = image.blob.read()
-#                 file_name = dir / image.name
-#                 images.append(file_name)
-#                 with file_name.open('wb') as f:
-#                     f.write(data)
-
-#             output = _optimize(images, parameters)
-#             local = BytesIO()
-#             world = BytesIO()
-
-#             np.save(local, output.local)
-#             np.save(world, output.world)
-
-#             shape = models.ShapeModel()
-#             shape.local.save('local.npy', ContentFile(local.getvalue()), save=False)
-#             shape.world.save('world.npy', ContentFile(world.getvalue()), save=False)
-#             shape.save()
-
-#             optimization.shape_model = shape
-#             optimization.save()
-
-#     except Exception:
-#         optimization.error = True
-#         optimization.save()
-#         raise
-#     finally:
-#         optimization.running = False
-#         optimization.save()
+from swcc.api import swcc_session
+from swcc.models import Dataset
 
 
-# @shared_task
-# def generate_groomed_segmentation_mesh(segmentation_id: int):
-#     segmentation: models.GroomedSegmentation = models.GroomedSegmentation.objects.get(
-#         pk=segmentation_id
-#     )
-
-#     vtp = nrrd_to_vtp(segmentation.blob.read())
-#     segmentation.mesh.save(segmentation.blob.name + '.vtp', ContentFile(vtp), save=False)
-#     segmentation.save(update_fields=['mesh'])
-
-
-# @shared_task
-# def generate_segmentation_mesh(segmentation_id: int):
-#     segmentation: models.Segmentation = models.Segmentation.objects.get(pk=segmentation_id)
-
-#     vtp = nrrd_to_vtp(segmentation.blob.read())
-#     segmentation.mesh.save(segmentation.blob.name + '.vtp', ContentFile(vtp), save=False)
-#     segmentation.save(update_fields=['mesh'])
+@shared_task
+def groom(user_id, dataset_id):
+    user = User.objects.get(id=user_id)
+    token, _created = Token.objects.get_or_create(user=user)
+    base_url = settings.API_URL
+    with swcc_session(base_url=base_url) as session:
+        session.set_token(token.key)
+        d = Dataset.from_id(dataset_id)
+        d.download('/tmp/ellipsoid_v0')
+    process = Popen(
+        ['/opt/shapeworks/bin/shapeworks', 'groom', '--name=/opt/django-project/'],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    print(process.communicate())
+    # TODO upload the groomed folder

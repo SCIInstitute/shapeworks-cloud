0d80f87a35e27795f41e3a442fb2149ea37ff00a
Anne Haley <anne.haley@kitware.com>
2022-09-15 15:21:24 -0400

    Add new (blank) component for Analysis tab

---
+++ web/shapeworks/src/components/AnalysisTab.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { selectedProject } from '@/store'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        console.log(selectedProject.value?.last_cached_analysis)
+    },
+})
+</script>
+
+<template>
+    <div class="pa-3">
+        Analysis content here
+         <v-expansion-panels :value="0">
+            <v-expansion-panel>
+                <v-expansion-panel-header>
+                    View PCA
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    slider goes here
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+                <v-expansion-panel-header>
+                    Charts
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    Charts go here
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </div>
+</template>

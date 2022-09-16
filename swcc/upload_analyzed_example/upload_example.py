e378a84664675057f5cb2fa58d7d1051470b5fd1
Anne Haley <anne.haley@kitware.com>
2022-09-16 11:54:05 -0400

    Lint fix

--- swcc/upload_analyzed_example/upload_example.py
+++ swcc/upload_analyzed_example/upload_example.py
@@ -1,10 +1,11 @@
 import getpass
 from pathlib import Path
+
 from swcc.api import swcc_session
 from swcc.models import Dataset, Project
 
-username = input("Enter username: ")
-password = getpass.getpass("Enter password: ")
+username = input('Enter username: ')
+password = getpass.getpass('Enter password: ')
 
 
 with swcc_session(base_url='http://localhost:8000/api/v1') as session:
@@ -16,7 +17,7 @@ with swcc_session(base_url='http://localhost:8000/api/v1') as session:
         name='Demo Data',
         description='An example dataset',
         license='No license',
-        acknowledgement='No acknowledgement'
+        acknowledgement='No acknowledgement',
     ).force_create()
 
     project_file = Path('project_demo/project_demo.swproj')
@@ -24,7 +25,7 @@ with swcc_session(base_url='http://localhost:8000/api/v1') as session:
         file=project_file,
         description='First project for demo data',
         dataset=dataset,
-        last_cached_analysis='analysis/project_demo_analysis.json'
+        last_cached_analysis='analysis/project_demo_analysis.json',
     ).create()
 
     print('Done.')

6c23efd6a11a4bd3854558316101eb8df4db8735
Anne Haley <anne.haley@kitware.com>
2022-10-13 08:15:08 -0400

    Signal deletion of cached analyses on deletion of project

--- shapeworks_cloud/core/apps.py
+++ shapeworks_cloud/core/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = 'shapeworks_cloud.core'
     verbose_name = 'ShapeWorks Cloud: Core'
+
+    def ready(self):
+        import shapeworks_cloud.core.signals  # NOQA

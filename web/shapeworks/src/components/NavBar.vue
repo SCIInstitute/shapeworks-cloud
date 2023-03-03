2e5894cdd94bce7b5d8481df9672021274e87188
Anne Haley <anne.haley@kitware.com>
2023-03-03 10:08:52 -0500

    Fix click on logo in navbar

--- web/shapeworks/src/components/NavBar.vue
+++ web/shapeworks/src/components/NavBar.vue
@@ -1,47 +1,53 @@
 <script lang="ts">
 import { defineComponent, computed } from '@vue/composition-api'
 import { logout, oauthClient } from '@/api/auth';
-import { selectedDataset } from '../store';
+import { selectedDataset, selectedProject } from '../store';
+import router from '@/router';
 
 
 export default defineComponent({
     setup() {
-        const params = computed(() => ({
-          dataset: selectedDataset.value?.id,
-        }))
+      const params = computed(() => ({
+        dataset: selectedDataset.value?.id,
+      }))
 
-        async function logInOrOut() {
-            if (oauthClient.isLoggedIn) {
-              await logout();
-              window.location.reload();
-            } else {
-              oauthClient.redirectToLogin();
-            }
-        }
+      async function logInOrOut() {
+          if (oauthClient.isLoggedIn) {
+            await logout();
+            window.location.reload();
+          } else {
+            oauthClient.redirectToLogin();
+          }
+      }
 
-        return {
-            oauthClient,
-            params,
-            logInOrOut,
-            selectedDataset,
-        }
+      function navigateToHome() {
+        selectedDataset.value = undefined
+        selectedProject.value = undefined
+        router.push('/')
+      }
+
+      return {
+          oauthClient,
+          params,
+          logInOrOut,
+          selectedDataset,
+          navigateToHome,
+      }
     }
 })
 </script>
 
 <template>
   <v-app-bar app height="50px">
-    <router-link to="/">
-      <div class="d-flex align-center px-5">
+    <div class="d-flex align-center px-5" @click="navigateToHome">
       <v-img
         alt="ShapeWorks Logo"
         src="favicon.ico"
         transition="scale-transition"
         width="55px"
       />
-        <v-toolbar-title class="text-h6">ShapeWorks</v-toolbar-title>
-      </div>
-    </router-link>
+      <v-toolbar-title class="text-h6">ShapeWorks</v-toolbar-title>
+    </div>
     <v-spacer />
     <v-btn
       v-if="oauthClient.isLoggedIn"

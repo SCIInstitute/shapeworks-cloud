d2f37962e434fb97ffb524ce04ca1babbea8635d
Anne Haley <anne.haley@kitware.com>
2023-03-29 10:16:59 -0400

    Write file structure comparison method

--- swcc/tests/test_download_upload.py
+++ swcc/tests/test_download_upload.py
@@ -2,10 +2,14 @@
 # This will fetch all datasets populated on the public server and attempt to
 # upload them all to the local server
 import random
+import os
+import filecmp
 from tempfile import TemporaryDirectory
 
 from swcc import models
 from swcc.api import swcc_session
+
+SAMPLE_SIZE = 3
 
 
 def project_as_dict_repr(project):
@@ -21,16 +25,38 @@ def project_as_dict_repr(project):
     return project_repr
 
 
+class dircmp(filecmp.dircmp):
+    def phase3(self):
+        fcomp = filecmp.cmpfiles(self.left, self.right, self.common_files, shallow=False)
+        self.same_files, self.diff_files, self.funny_files = fcomp
+
+
+def is_same(dir1, dir2):
+    compared = dircmp(dir1, dir2)
+    different = (
+        compared.left_only or compared.right_only or compared.diff_files or compared.funny_files
+    )
+    if different:
+        return False
+    for subdir in compared.common_dirs:
+        if not is_same(os.path.join(dir1, subdir), os.path.join(dir2, subdir)):
+            return False
+    return True
+
+
 def test_download_upload_cycle(session):
-    with TemporaryDirectory() as download_dir:
+    with TemporaryDirectory() as temp_dir:
         all_projects = []
 
+        # Download from public server
+        download_dir = f'{temp_dir}/download'
         with swcc_session() as public_server_session:
             public_server_session.login('testuser@noemail.nil', 'cicdtest')
-            all_projects = random.sample(list(models.Project.list()), 1)
+            all_projects = random.sample(list(models.Project.list()), SAMPLE_SIZE)
             for project in all_projects:
                 project.download(download_dir)
 
+        # Upload to local server
         for project in all_projects:
             d = models.Dataset(
                 name=project.dataset.name,
@@ -46,6 +72,14 @@ def test_download_upload_cycle(session):
                 file=project.file.path,
                 description=project.description,
             ).create()
+
+        # Redownload from local server
+        redownload_dir = f'{temp_dir}/redownload'
+        for project in all_projects:
+            project.download(redownload_dir)
+
+        # Test for file structure congruence
+        assert is_same(download_dir, redownload_dir)
 
     for local_project, remote_project in zip(models.Project.list(), all_projects):
         local_repr = project_as_dict_repr(local_project)

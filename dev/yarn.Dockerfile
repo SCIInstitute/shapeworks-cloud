41dfe2ce8278a20d96591f737ca521cf10864110
Anne Haley <anne.haley@kitware.com>
2023-08-21 17:38:09 -0400

    Write a makeclient management command

--- dev/yarn.Dockerfile
+++ dev/yarn.Dockerfile
@@ -2,4 +2,3 @@ FROM node:latest
 
 WORKDIR /app/server
 RUN yarn install
-CMD yarn serve
---
+++ shapeworks_cloud/core/management/commands/makeclient.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import User
+from oauth2_provider.models import Application
+from django.core.management.base import BaseCommand, CommandError
+
+
+DEFAULT_REDIRECT_URI = 'http://localhost:8081/'
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-u", "--uri", type=str)
+
+    def handle(self, uri, **options):
+        if not uri:
+            uri = DEFAULT_REDIRECT_URI
+
+        try:
+            user = User.objects.first()
+            if Application.objects.filter(user=user).exists():
+                raise CommandError(
+                    'The client already exists. You can administer it from the admin console.'
+                )
+                return
+            application = Application(
+                name='client-app',
+                client_secret='',
+                client_type='public',
+                redirect_uris=uri,
+                authorization_grant_type='authorization-code',
+                user=user,
+                skip_authorization=True,
+            )
+            application.save()
+
+            with open('/client/dev/yarn.env', 'w') as f:
+                f.write(f'VUE_APP_OAUTH_CLIENT_ID={application.client_id}')
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Restart docker containers to complete change. Client will be available at {uri}.'
+                )
+            )
+        except User.DoesNotExist:
+            raise CommandError(
+                'A user must exist before creating a client. Use createsuperuser command.'
+            )

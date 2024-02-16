9b1e3665c129a3c7626662874dd80c47a8893a86
Anne Haley <anne.haley@kitware.com>
2024-02-16 16:59:00 +0000

    When setting a constraint location, reassign allSetConstraints for changesMade watcher

--- web/shapeworks/src/store/methods.ts
+++ web/shapeworks/src/store/methods.ts
@@ -519,6 +519,7 @@ export function setConstraintLocation(subject, item, location) {
     if (!allSetConstraints.value[subject.name]) allSetConstraints.value[subject.name] = {}
     if (!allSetConstraints.value[subject.name][item.domain]) allSetConstraints.value[subject.name][item.domain] = {}
     allSetConstraints.value[subject.name][item.domain][item.id] = location
+    allSetConstraints.value = Object.assign({}, allSetConstraints.value)
     reassignConstraintNumSetValues()
 }
 

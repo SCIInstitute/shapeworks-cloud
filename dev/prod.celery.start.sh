c830c34363809d0ab9111c4516459f0fc864a55b
Anne Haley <anne.haley@kitware.com>
2024-03-13 20:34:05 +0000

    fix: Update celery launch command in start script

--- dev/prod.celery.start.sh
+++ dev/prod.celery.start.sh
@@ -2,4 +2,4 @@ cd ~/celery_project
 
 for keyval in $(cat .env | sed -e 's/: /=/g' -e "s/'\|,\|{\|}//g" -e 's/", "/ /g' -e 's/"}//g' ); do export $keyval; done
 
-celery -A shapeworks_cloud.celery worker -n $HOSTNAME -Q gpu
+celery -A shapeworks_cloud.celery worker -n "w1@${HOSTNAME}" -Q gpu --logfile=celery-logs

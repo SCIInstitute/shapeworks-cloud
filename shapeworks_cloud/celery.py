74a71aef138bdeb9966ccd57fb99edc7dd705faa
Anne Haley <anne.haley@kitware.com>
2024-03-21 17:17:51 +0000

    Undo manual queue definition

--- shapeworks_cloud/celery.py
+++ shapeworks_cloud/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from kombu import Queue
 import configurations.importer
 
 from .manage_workers import manage_workers
@@ -15,12 +14,8 @@ configurations.importer.install()
 
 # Using a string config_source means the worker doesn't have to serialize
 # the configuration object to child processes.
-app = Celery(
-    config_source='django.conf:settings',
-    namespace='CELERY',
-    task_queues=(
-    Queue('celery'), Queue('gpu', durable=False)
-))
+app = Celery(config_source='django.conf:settings', namespace='CELERY')
+
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):

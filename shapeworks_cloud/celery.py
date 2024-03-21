6e87ace3d24fa1c2421ebfae34da0c9f7a9e9758
Anne Haley <anne.haley@kitware.com>
2024-03-21 16:22:18 +0000

    Fix GPU queue definition

--- shapeworks_cloud/celery.py
+++ shapeworks_cloud/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from kombu import Queue
 import configurations.importer
 
 from .manage_workers import manage_workers
@@ -14,8 +15,12 @@ configurations.importer.install()
 
 # Using a string config_source means the worker doesn't have to serialize
 # the configuration object to child processes.
-app = Celery(config_source='django.conf:settings', namespace='CELERY')
-
+app = Celery(
+    config_source='django.conf:settings',
+    namespace='CELERY',
+    task_queues=(
+    Queue('celery'), Queue('gpu', durable=False)
+))
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):

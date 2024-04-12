1795fa227d57e3d0ed8f6b3630b4cd5e0f8548e6
Anne Haley <anne.haley@kitware.com>
2024-04-12 22:06:18 +0000

    fix: create and activate shapeworks conda env on worker

--- dev/prod.celery.start.sh
+++ dev/prod.celery.start.sh
@@ -4,4 +4,5 @@ cd ~/celery_project
 
 for keyval in $(cat .env | sed -e 's/: /=/g' -e "s/'\|,\|{\|}//g" -e 's/", "/ /g' -e 's/"}//g' ); do export $keyval; done
 
+conda activate shapeworks
 celery -A shapeworks_cloud.celery worker -n "w1@${HOSTNAME}" -Q gpu --logfile=celery-logs

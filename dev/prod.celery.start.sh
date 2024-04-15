d084be4063d42d9ceceb4f921da654f30b2eee84
Anne Haley <anne.haley@kitware.com>
2024-04-15 20:31:55 +0000

    Add `-P threads` to the prod celery startup script

--- dev/prod.celery.start.sh
+++ dev/prod.celery.start.sh
@@ -5,4 +5,4 @@ cd ~/celery_project
 for keyval in $(cat .env | sed -e 's/: /=/g' -e "s/'\|,\|{\|}//g" -e 's/", "/ /g' -e 's/"}//g' ); do export $keyval; done
 
 conda activate shapeworks
-celery -A shapeworks_cloud.celery worker -n "w1@${HOSTNAME}" -Q gpu --logfile=celery-logs
+celery -A shapeworks_cloud.celery worker -n "w1@${HOSTNAME}" -Q gpu -P threads --logfile=celery-logs
